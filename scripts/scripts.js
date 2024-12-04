d9b24fd150ed6057615f4c1312bd4e90cdf276d4
Carlos Osorio <carlosoc@live.com>
2024-12-03 20:34:30 -0600

    Hicimos el Header y el cookie banner de starbacks

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+console.log("Jajajaja")
+
+const closeButton = document.getElementById('closeBannerButton')
+const cookieBanner = document.getElementById('cookieBanner')
+closeButton.addEventListener('click', () => {
+  cookieBanner.classList.add('invisible')
+})
